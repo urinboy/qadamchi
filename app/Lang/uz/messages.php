6d585d87dc52def2a77f40517ad4fa885c87c193
urinboy <urinboytursunboev@gmail.com>
2026-07-15 22:35:38 +0500

    app: o'zbekcha til fayllari qo'shildi (Lang/uz)

---
+++ app/Lang/uz/messages.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'welcome' => 'Xush kelibsiz, :name!',
+    'login_failed' => 'Email yoki parol noto\'g\'ri.',
+    'created' => 'Muvaffaqiyatli yaratildi.',
+];
---
+++ app/Lang/uz/validation.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'required'  => ':attribute maydoni majburiy.',
+    'email'     => ':attribute to\'g\'ri email bo\'lishi kerak.',
+    'min'       => ':attribute kamida :min belgi bo\'lishi kerak.',
+    'max'       => ':attribute ko\'pi bilan :max belgi bo\'lishi kerak.',
+    'unique'    => ':attribute allaqachon ishlatilgan.',
+    'confirmed' => ':attribute tasdiqlash mos kelmadi.',
+];
